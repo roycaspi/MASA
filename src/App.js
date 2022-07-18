505634b4b31876b4e02896977c8c3fb9fb1c68aa
RoyCaspi <caspi7@hotmail.com>
2022-07-18 14:22:52 +0300

    final

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ import { Container } from "react-bootstrap"
 import { AuthProvider } from "./contexts/AuthContext"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-// roy was here - thank god!!
-// udi was here
 
 function App() {
   return (
